1f2baf8b14e4f4c28c5e43aeec3f6b6830a42107
Kardexiaeducacion <kardexia.educacion@gmail.com>
2026-05-28 19:23:48 -0600

    fix_next_image_domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rmdmrtsbbymorsumijph.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
